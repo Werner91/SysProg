1c23cf5d9e89b199962838d84c9ca37b0da802e0
as-131866 <as-131866@ap055018.hs-weingarten.de>
2015-05-26 12:11:25 +0200

    server angefangen

--- server/main.c
+++ server/main.c
@@ -8,6 +8,18 @@
  */
 
 #include "common/util.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket>
+#include <netinet/in.h>
+
+
+
+void error(char *msg){ //wird aufgerufen, wenn ein Systemcall fehlschlägt
+	perror(msg);
+	exit(1);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -15,6 +27,18 @@ int main(int argc, char **argv)
 	/* debugEnable() */
 
 	infoPrint("Server Gruppe 4 (Stroh, Steinbinder, Strohm)");
+
+
+	int sockfd, newsockfd, portno, clillen, n;
+	char buffer[256]; //gelesene Character werden hier gespeichert (also die Nachricht)
+
+	struct sockaddr_in serverAddr, clientAddr; //Struktur enthält die Internet-Adresse des Servers und des Clients
+
+	if(argc < 2){
+		fprintf(stderr, "Fehler, keine Portnummer eingegeben\n");
+		exit(1);
+	}
+
 
 
 	return 0;
