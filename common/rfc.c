a2815886efc1f4892ba2751bb5f2c08883a8c369
ms-131871 <ms-131871@ap055020.hs-weingarten.de>
2015-05-28 11:22:48 +0200

    LRQ LOK and reading functions added

--- common/rfc.c
+++ common/rfc.c
@@ -12,3 +12,49 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "rfc.h"
+
+// KontrollFunktion um Paketempfang zu ueberpruefen
+// Rueckgabe -1 bei Fehler Paketempfang
+// Rueckgabe  0 falls kein Paket verfuegbar bzw Verbindung unterbrochen
+// Rueckgabe  1 falls Paketuebertragung erfolgreich war
+int receiveMessage(int socket, rfc *packet) {
+	void* packetbuffer = packet;
+	// recv - receive a message from a connected socket
+	// recv(int socket, void *buffer, size_t length, int flags);
+	int receivePacket = recv(socket, packetbuffer, RFC_BASE_SIZE, 0);
+	if (receivePacket == -1) {
+		error("rfc.c/receiveMessage: Fehler beim Paketempfang I"); // Laut Anleitung errno
+		return -1;
+	}
+	if (receivePacket == 0) {
+		// Keine Pakete verfuegbar oder Verbindung unterbrochen
+		return 0;
+	}
+
+	int packetLength = ntohs(packet->base.length);
+
+	if (packetLength > 0) {
+		receivePacket = recv(socket, packetbuffer + RFC_BASE_SIZE, packetLength, 0);
+		if (receivePacket == -1) {
+			error("rfc.c/receiveMessage: Fehler beim Paketempfang II"); // Laut Anleitung errno
+			return -1;
+		}
+		if (receivePacket == 0) {
+			// Keine Pakete verfuegbar oder Verbindung unterbrochen
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+// KontrollFunktion fuer das Type-Feld des jeweiligen Datenpakets
+// Rueckgabe 1 bei Uebereinstimmung des Type-Felds
+// Rueckgabe 0 bei falschem Type-Feld
+int typeControl(struct rfcBase base, uint8_t* type) {
+	if (base.type == *type) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
--- common/rfc.h
+++ common/rfc.h
@@ -13,6 +13,59 @@
 
 #include "common/question.h"
 
-/* ... */
+// Padding durch Compiler verhindern
+#pragma pack(push, 1)
+
+#define RFC_BASE_SIZE 3
+#define RFC_VERSION 7
+
+// Basis-Struktur der RFC
+struct rfcBase {
+	uint8_t type;
+	uint16_t length;
+};
+
+// LRQ LoginRequest Struktur
+// Type = 1
+// Length = Länge des Namens + 1
+// RFCVersion = 7
+// Login-Name, UTF-8, nicht nullterminiert, maximal 31 Bytes
+#define RFC_LRQ_SIZE (RFC_BASE_SIZE + 1)
+struct rfcLoginRequest {
+	struct rfcBase base;
+	uint8_t rfcVersion;
+	char loginName[31];
+};
+
+// LOK LoginResponseOk Struktur
+// Type = 2
+// Length = 2
+// RFCVersion = 7
+// ClientID = 0 (0 := Spielleiter)
+#define RFC_LOK_SIZE (RFC_BASE_SIZE + 2)
+struct rfcLoginResponseOk {
+	struct rfcBase base;
+	uint8_t rfcVersion;
+	uint8_t clientID;
+};
+
+typedef union {
+	struct rfcBase base;
+	struct rfcLoginRequest lrq;
+	struct rfcLoginResponseOk lok;
+} rfc;
+
+// Ende Pragma Block
+#pragma pack(pop)
+
+
+/* ******* Block fuer Funktions-Prototypen ******* */
+/* *********************************************** */
+
+// KontrollFunktion um Paketempfang zu ueberpruefen
+int receiveMessage(int socket, rfc *packet);
+
+// KontrollFunktion fuer das Type-Feld des jeweiligen Datenpakets
+int typeControl(struct rfcBase base, uint8_t* type);
 
 #endif
